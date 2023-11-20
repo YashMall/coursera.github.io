48a4a1ef4d226a23737745d44afc2968083d7d00
Yash Mall <125197531+YashMall@users.noreply.github.com>
2023-11-20 18:27:00 +0530

    Add files via upload
    Updated solution

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,66 +1,59 @@
-(
-    function() {
-        angular.module('checkOffApp', [])
-        .controller('toBuyController', toBuyController)
-        .controller('alreadyBoughtController', alreadyBoughtController)
-        .service('buyBoughtServer', buyBoughtServer);
+(function(){
 
-        toBuyController.$inject = ['buyBoughtServer'];
-        function toBuyController(buyBoughtServer) {
-            var toBuy = this;
-            toBuy.items = [
-                            {
-                                name:'litres of milk',
-                                quantity: 3
-                            },
-                            {
-                                name: 'kilos of apples',
-                                quantity: 2
-                            },
-                            {
-                                name: 'eggs',
-                                quantity: 5
-                            },
-                            {
-                                name: 'toilet rolls',
-                                quantity:4
-                            },
-                            {
-                                name: 'kilos of bananas',
-                                quantity: 3
-                            },
-                            {
-                                name: 'litres of orange juice',
-                                quantity: 4
-                            }
-                          ];
-            toBuy.moveItem = function(itemIndex) {
-                buyBoughtServer.moveItem(itemIndex, toBuy.items);
-            }
-
-            
-        };
-
-        alreadyBoughtController.$inject = ['buyBoughtServer'];
-        function alreadyBoughtController(buyBoughtServer) {
-          var alreadyBought = this;  
-          alreadyBought.items = buyBoughtServer.items;
-        };
-
-        function buyBoughtServer() {
-            var buyBought = this;
-            buyBought.items = [];
-            buyBought.moveItem = function (itemIndex, arrayOfitems) {
-                var item = {
-                    name: '',
-                    quantity: ''
-                }
-                item.name = arrayOfitems[itemIndex].name;
-                item.quantity = arrayOfitems[itemIndex].quantity;
-                arrayOfitems.splice(itemIndex, 1);
-                buyBought.items.push(item);
-            };
-
-        };
+    'use strict';
+    
+    angular.module('LunchCheck',[])
+    
+    .controller('LunchCheckController',LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController ($scope){
+    
+    $scope.lunch = '' ;
+    $scope.message = '' ;
+    $scope.color = '';
+    $scope.checkLunch = function(){
+      var foodNum = calcFoodNumber($scope.lunch);
+      if ($scope.lunch == ''){
+        $scope.message = "Please enter data first";
+        $scope.color = {"color" : "red"} ;
+      }
+      else if(foodNum > 3){
+        $scope.message = "Too much!";
+        $scope.color = {"color" : "green"} ;
+      }
+      else if(foodNum <= 3){
+        $scope.message = "Enjoy!";
+        $scope.color = {"color" : "green"} ;
+      }
+    
     }
-)();
+    
+    function calcFoodNumber(foods)
+    {
+      var numOfFood = 0;
+      var foodElem = [];
+      foodElem = foods.split(',');
+      for(var i = 0 ; i<foodElem.length ; i++){
+        if(checkEmpty(foodElem[i])){
+          numOfFood++ ;
+        }
+      }
+    
+      return numOfFood ;
+    }
+    
+    function checkEmpty(string)
+    {
+        for(var i = 0 ; i < string.length ; i++)
+          if(string[i] && string[i] != " " )
+            return true ;
+        return false ;
+    }
+    
+    
+    
+    }
+    
+    
+    
+    })();
